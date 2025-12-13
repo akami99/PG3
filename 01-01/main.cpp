9846b229749ee02b5acc9de909a76aee9c06eff7
akami99 <k024g0059@g.neec.ac.jp>
2025-12-13 12:20:17 +0900

    PG3_01-01.geminiにてLinterエラーのチェックを完了

--- 01-01/main.cpp
+++ 01-01/main.cpp
@@ -1,7 +1,7 @@
 
+#include <cstring> // strcmp用
 #include <iostream>
 #include <list>
-using namespace std;
 
 int main() {
   // 1970,2019,2022年の山手線の駅名をそれぞれlistに格納
@@ -9,7 +9,7 @@ int main() {
   int count = 0; // カウンタを初期化
 
   // 初めは1970年の駅名
-  list<const char *> JY = {
+  std::list<const char *> JY = {
       "Tokyo",        "Kanda",     "Akihabara", "Okachimachi",  "Ueno",
       "Uguisudani",   "Nippori",   "Tabata",    "Komagome",     "Sugamo",
       "Otsuka",       "Ikebukuro", "Mejiro",    "Takadanobaba", "Shin-Okubo",
@@ -17,51 +17,53 @@ int main() {
       "Meguro",       "Gotanda",   "Osaki",     "Shinagawa",    "Tamachi",
       "Hamamatsucho", "Shimbashi", "Yurakucho"};
 
-  printf_s("1970年の山手線の駅\n");
-  for (auto itr = JY.begin(); itr != JY.end(); ++itr) {
+  std::cout << "1970年の山手線の駅\n";
+  for (const auto &station : JY) {
     // 番号を表示(JY1:)
-    cout << " JY" << ++count << ":";
+    std::cout << " JY" << ++count << ":";
 
     // 要素を表示
-    cout << *itr << "\n";
+    std::cout << station << "\n";
   }
 
   // 2019年の駅名に更新(NipporiとTabataの間に追加)
   for (auto itr = JY.begin(); itr != JY.end(); ++itr) {
-    if (*itr == "Tabata") {
-      JY.insert(itr, "Nishi-Nippori");
-      ++itr;
+    if (std::strcmp(*itr, "Tabata") == 0) {
+      // 戻り値を使用しないことを明示的に示す
+      [[maybe_unused]] auto new_itr = JY.insert(itr, "Nishi-Nippori");
+      break;
     }
   }
 
   count = 0; // カウンタをリセット
 
-  printf_s("\n2019年の山手線の駅\n");
-  for (auto itr = JY.begin(); itr != JY.end(); ++itr) {
+  std::cout << "\n2019年の山手線の駅\n";
+  for (const auto &station : JY) {
     // 番号を表示(JY1:)
-    cout << " JY" << ++count << ":";
+    std::cout << " JY" << ++count << ":";
 
     // 要素を表示
-    cout << *itr << "\n";
+    std::cout << station << "\n";
   }
 
   // 2022年の駅名に更新(ShinagawaとTamachiの間に追加)
   for (auto itr = JY.begin(); itr != JY.end(); ++itr) {
-    if (*itr == "Tamachi") {
-      JY.insert(itr, "Takanawa Gateway");
-      ++itr;
+    if (std::strcmp(*itr, "Tamachi") == 0) {
+      // 戻り値を使用しないことを明示的に示す
+      [[maybe_unused]] auto new_itr = JY.insert(itr, "Takanawa Gateway");
+      break;
     }
   }
 
   count = 0; // カウンタをリセット
 
-  printf_s("\n2022年の山手線の駅\n");
-  for (auto itr = JY.begin(); itr != JY.end(); ++itr) {
+  std::cout << "\n2022年の山手線の駅\n";
+  for (const auto &station : JY) {
     // 番号を表示(JY1:)
-    cout << " JY" << ++count << ":";
+    std::cout << " JY" << ++count << ":";
 
     // 要素を表示
-    cout << *itr << "\n";
+    std::cout << station << "\n";
   }
 
   return 0;
